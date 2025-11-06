6fcf52f0346eee555d41290c38290ab2abe846da
robert-stevenson-1 <78357805+robert-stevenson-1@users.noreply.github.com>
2025-11-06 15:06:31 +0000

    added script with command to auto generate py from ui file

---
+++ HSIRig/ui/generate_ui.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Source - https://stackoverflow.com/questions/43028904/converting-ui-to-py-with-python-3-6-on-pyqt5
+# Posted by Danilo Gasques
+# Retrieved 2025-11-06, License - CC BY-SA 4.0
+#
+# Modified by Robert Stevenson
+# Created 6 Nov 2025
+
+
+python3 -m PyQt5.uic.pyuic -x ./main_window.ui -o main_window.py
